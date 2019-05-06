e0106f691f1a10e658cf9c18af7c573a710d3bbb
Tim Sassen <t.sassen@xsarus.nl>
2019-05-06 11:01:33 +0200

    Fix typo

--- Helper/API/Attachment.php
+++ Helper/API/Attachment.php
@@ -1,10 +1,12 @@
 <?php
 namespace Flowmailer\M2Connector\Helper\API;
-	class Attachment {
-		public $content;
-		public $contentId;
-		public $contenType;
-		public $filename;
-	}
-	
+
+class Attachment
+{
+    public $content;
+    public $contentId;
+    public $contentType;
+    public $filename;
+}
+
 ?>
